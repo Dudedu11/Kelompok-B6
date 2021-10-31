89c6f2e0f9a08b344c9742bc4542d6085763b283
HanhanSeptians <hanhan.septian.tif20@polban.ac.id>
2021-10-31 21:13:32 +0700

    rapiin data

--- front-end/likeComment/src/assets/icons/index.js
+++ front-end/likeComment/src/assets/icons/index.js
@@ -7,14 +7,7 @@ import IconSafety from './Diamond.svg';
 import IconSafetyActive from './diamondAktif.svg';
 import IconProfil from './profile.svg';
 import IconProfilActive from './profileAktif.svg';
-import IconAdd from './add.svg';
-import IconArrowLeft from './arrowLeft.svg';
-import IconDelete from './delete.svg';
-import IconEdit from './edit.svg';
-import IconManage from './manage.svg';
-import IconPesan from './pesan.svg';
-import IconBell from './bell.svg';
-import IconDel from './del.svg';
 
-export { IconFeed, IconSearch, IconToolbar, IconSafety, IconProfil, IconProfilActive, IconSafetyActive, IconSearchActive, IconFeedActive, IconDelete, IconArrowLeft, IconEdit, IconAdd, IconBell, IconManage, IconPesan, IconDel }
+
+export { IconFeed, IconSearch, IconToolbar, IconSafety, IconProfil, IconProfilActive, IconSafetyActive, IconSearchActive, IconFeedActive }
 
